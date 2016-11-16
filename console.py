c65f42202ff7399d78f1bd8c0dc57df074e7ccb3
JonasUJ <jonasuj@hotmail.dk>
2016-11-16 20:15:57 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,9 +1,12 @@
 import re, sys, os, fp
 
+NAME_OF_FILE = os.path.basename(sys.argv[0])
+DIRECTORY_OF_FILE = sys.argv[0].rstrip(NAME_OF_FILE)
+
 class Handler:
 
     def __init__(self):
-        self.MAX_CMD_ARGS = 5
+        self.MAX_CMD_ARGS = 10 # There'll be one less
         self.COMMAND_PATTERN = re.compile(r'([a-zA-Z]+)' + r''.join([r'(?: (?P<quote%s>\")?((?(quote%s)[a-zA-Z0-9_.+*/!#&/()=?~<> -]+|[a-zA-Z0-9_.+*/!#&/()=?~<>-]+))(?(quote%s)"))?' % (x, x, x) for x in range(self.MAX_CMD_ARGS)]))
         self.COMMAND_PROMPT = "\n~ "
         self.COMMANDS = {'exit':self.do_exit, 'cal':self.do_cal, 'say':self.do_say, 'help':self.do_help}
@@ -11,20 +14,20 @@ class Handler:
     def default(self, *args):
         print("Error,", *args)
 
-    def do_help(self, arg_dict):
-        help_dict = fp.parse_file(r'C:\Users\jonas\Desktop\PythonProjects\consolepyhelp.txt')
+    def do_help(self, **kwargs):
+        help_dict = fp.parse_file(DIRECTORY_OF_FILE + 'consolepyhelp.txt')
         for cmd in help_dict['help'].keys():
             print('%s - %s' % (cmd, help_dict['help'][cmd]))
     
-    def do_exit(self, arg_dict):
+    def do_exit(self, **kwargs):
         sys.exit(1)
 
-    def do_say(self, arg_dict):
-        print(arg_dict['arg1'])
-
-    def do_cal(self, arg_dict):
+    def do_say(self, **kwargs):
+        print(kwargs['arg1'])
+ 
+    def do_cal(self, **kwargs):
         try:
-            print(eval(arg_dict['arg1']))
+            print(eval(kwargs['arg1']))
         except Exception as e:
             self.default(e)
 
@@ -40,14 +43,11 @@ class Handler:
             if match:
                 parsed_inst = self.parse_groups(match.groups())
                 try:
-                    self.COMMANDS[parsed_inst['cmd']](parsed_inst)
-                except Exception as e:
+                    self.COMMANDS[parsed_inst['cmd']](**parsed_inst)
+                except KeyError as e:
                     self.default('Invalid command', e)
             else:
                 self.default("match =", match)
 
 console = Handler()
 console.main()
-
-
-
