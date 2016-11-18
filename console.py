93a4e71611a4f6ceb29301bee377b26576b1341c
JonasUJ <jonasuj@hotmail.dk>
2016-11-18 22:49:39 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,16 +1,20 @@
 import re, sys, os, fp
+from ber2 import dictdb
 
 class Handler:
 
     def __init__(self):
         self.NAME_OF_FILE = os.path.basename(sys.argv[0])
         self.DIRECTORY_OF_FILE = sys.argv[0].rstrip(self.NAME_OF_FILE)
-        self.MAX_CMD_ARGS = 2 # There'll be one less, since it counts the command as one
-        self.COMMAND_PATTERN_UNCOMPILED = r'([a-zA-Z]+)' + r''.join([r'(?: (?P<quote%s>\"|~)?((?(quote%s)(?:\\"|[a-zA-Z0-9_.+*/!#&/()=?~<> -])+|[a-zA-Z0-9_.+*/!#&/()=?~<>"-]+))(?(quote%s)(?P=quote%s)))?' % (x, x, x, x) for x in range(self.MAX_CMD_ARGS)])
+        self.MAX_CMD_ARGS = 5 # There'll be one less, since it counts the command as one
+        self.COMMAND_PATTERN_UNCOMPILED = r'([a-zA-Z]+)' + r''.join([r'(?: (?P<quote%s>\"|~)?((?(quote%s)(?:\\"|[a-zA-Z0-9_.+*/!#&/()=?~<>: -])+|[a-zA-Z0-9_.+*/!#&/()=?~<>:"-]+))(?(quote%s)(?P=quote%s)))?' % (x, x, x, x) for x in range(self.MAX_CMD_ARGS)])
         self.COMMAND_PATTERN = re.compile(self.COMMAND_PATTERN_UNCOMPILED)
         self.EMBEDDED_COMMAND_PATTERN = re.compile('~' + self.COMMAND_PATTERN_UNCOMPILED + '~')
+        self.PATH_PATTERN = re.compile(r'^[a-zA-Z]:/(?:[^\\/:*?"<>|\r\n]+/)*[^\\/:*?"<>|\r\n]*$')
         self.COMMAND_PROMPT = "\n~ "
-        self.COMMANDS = {'exit': self.do_exit, 'quit': self.do_exit, 'cal': self.do_cal, 'say': self.do_say, 'help': self.do_help, 'py': self.do_py}
+        self.RESERVED_NAMES = []
+        self.COMMANDS = {'exit': self.do_exit, 'quit': self.do_exit, 'cal': self.do_cal, 'say': self.do_say, 'help': self.do_help, 'py': self.do_py, 'create': self.do_create, 'set': self.do_set, 'get': self.do_get}
+        self.save_dict = dictdb(self.DIRECTORY_OF_FILE + 'save.txt')
 
     def default(self, error, restart):
         if restart: 
@@ -26,6 +30,7 @@ class Handler:
         return ''
     
     def do_exit(self, **kwargs):
+        self.save_dict.save()
         sys.exit(1)
         return 'exited (unless you\'re seeing this)'
 
@@ -48,6 +53,43 @@ class Handler:
     def do_py(self, **kwargs):
         os.system('py')
         return ''
+    
+    def do_create(self, **kwargs):
+        try:
+            if self.PATH_PATTERN.match(kwargs['arg1']):
+                try:
+                    with open(kwargs['arg1'], 'w') as f:
+                        return ''
+                except PermissionError:        
+                    return 'Couldn\'t create ' + kwargs['arg1']
+            elif kwargs['arg2'] == None and kwargs['arg1'] != None:
+                try:
+                    with open(self.save_dict['standard_dir'] + kwargs['arg1'], 'w') as f:
+                        return ''
+                except PermissionError:        
+                    return 'Couldn\'t create ' + kwargs['arg1']
+            elif self.PATH_PATTERN.match(kwargs['arg2']):
+                try:
+                    with open(kwargs['arg2'] + kwargs['arg1'], 'w') as f:
+                        return ''
+                except PermissionError:        
+                    return 'Couldn\'t create ' + kwargs['arg1']
+        except TypeError:
+            return 'No input'
+
+    def do_set(self, **kwargs):
+        if kwargs['arg1'] not in self.RESERVED_NAMES:
+            self.save_dict[kwargs['arg1']] = kwargs['arg2']
+            self.save_dict.save()
+            return ''
+        else:
+            return '\'' + kwargs['arg1'] + '\' is a reserved name and can\'t be changed'
+
+    def do_get(self, **kwargs):
+        try:
+            return self.save_dict[kwargs['arg1']]
+        except KeyError as e:
+            return 'Couldn\'t find ' + e.__str__()
 
     def parse_groups(self, groups):
         result = [('cmd', groups[0])]
@@ -84,7 +126,7 @@ class Handler:
                     return parsed_inst['arg%s' % i]
                 except AttributeError:
                     return self.default('Invalid imbedded command', True)
-            
+            '''
             if self.COMMAND_PATTERN.match(parsed_inst['arg%s' % i]) and self.COMMAND_PATTERN.match(parsed_inst['arg%s' % i]).group(1) in self.COMMANDS:
                 #print('command ', self.COMMAND_PATTERN.match(parsed_inst['arg%s' % i]).group(1))
                 #print('got invoked by ', parsed_inst)
@@ -94,7 +136,7 @@ class Handler:
                 parsed_inst['arg%s' % i] = list(parsed_inst['arg%s' % i])
                 parsed_inst['arg%s' % i][new_match.span()[0]: new_match.span()[1]] = list(str(selfcall_outcome))
                 parsed_inst['arg%s' % i] = ''.join(parsed_inst['arg%s' % i])      
-            
+            '''
         try:
             #print('before call ', parsed_inst)
             return self.COMMANDS[parsed_inst['cmd']](**parsed_inst)
